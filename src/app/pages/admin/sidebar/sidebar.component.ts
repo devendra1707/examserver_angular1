34c2e7210f2357972f874e7c4e2720b6f65f8484
devendra1707 <pdevendra033@gmail.com>
2023-03-05 16:29:25 +0530

    update

--- src/app/pages/admin/sidebar/sidebar.component.ts
+++ src/app/pages/admin/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  isLogIn = true;
+  user = null;
+
+  constructor(public login: LoginService) { }
+
+  public logout() {
+    this.login.logout();
+    this.isLogIn = false;
+    // this.user = null;
+    window.location.reload();
+
+    // this.login.loginStatusSubject.next(false);
+  }
 }
